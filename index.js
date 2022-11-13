15ff7c67eaf9e2f45b46fc819c5c42c961ed4e5a
Jay <j.thomastt1@gmail.com>
2022-11-13 17:03:48 -0600

    Set up SQL connection

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Dependencies
-const mySql = require('mysql2');
+const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require ('inquirer');
+
+// Connection to SQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 6000,
+    user: 'user',
+    password: 'password',
+    database: 'Employees'
+ });
+
