5edfc0d3e2b07ff40e7e72a07900274d154db47b
Jay <j.thomastt1@gmail.com>
2022-11-13 18:41:28 -0600

    added main menu for options

--- index.js
+++ index.js
@@ -6,9 +6,60 @@ const inquirer = require ('inquirer');
 // Connection to SQL
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 6000,
+    port: 2002,
     user: 'user',
     password: 'password',
     database: 'Employees'
  });
 
+ connection.connect();
+
+// Added main directory
+const choiceEmp = () => 
+    inquirer.prompt ([
+        {
+            type:"list",
+            name:"Directory",
+            message: "Please choose what you would like to do?",
+            choices:["View Employees", "View Departments","View Roles","Enter Employee","Enter Departments", "Enter Roles","Change Employee Role", "Exit"]        
+        }])
+
+.then((answer) => {
+
+    switch(answer.Directory){
+      case "View all Employees":
+        viewAllEmployees();
+        break;
+      
+      case "View Departments":
+        viewDepartments();
+        break;
+
+      case "View Roles":
+        viewRoles();
+        break;  
+
+      case "Add Employee":
+        enterEmployee();
+        break;  
+
+      case "Add Departments":
+        enterDepartment();
+        break;  
+
+      case "Add Roles":
+        enterRoles();
+        break;
+
+      case "Update Employee Role":
+        changeEmployeeRole();
+        break;
+
+         case "Exit":
+        con = false;
+        break;
+        
+    }
+    
+  }); 
+choiceEmp();
